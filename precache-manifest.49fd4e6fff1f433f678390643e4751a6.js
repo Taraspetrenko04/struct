196be60b596610738a9fef36d9686be31697e157
unknown <Taraspetrenko04@gmail.com>
2020-04-10 15:12:29 +0300

    Updates

---
+++ precache-manifest.49fd4e6fff1f433f678390643e4751a6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0658ff938401f5ab6e85bcf6e8664b2f",
+    "url": "/struct/index.html"
+  },
+  {
+    "revision": "a42c22fb96518e6dc027",
+    "url": "/struct/static/css/2.8fc03f2f.chunk.css"
+  },
+  {
+    "revision": "0eca878074e130792d1d",
+    "url": "/struct/static/css/main.998bc056.chunk.css"
+  },
+  {
+    "revision": "a42c22fb96518e6dc027",
+    "url": "/struct/static/js/2.ba92afc8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/struct/static/js/2.ba92afc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eca878074e130792d1d",
+    "url": "/struct/static/js/main.6b22c008.chunk.js"
+  },
+  {
+    "revision": "37a04a1d14913554074f",
+    "url": "/struct/static/js/runtime-main.36eb14ab.js"
+  },
+  {
+    "revision": "ac0bace5af824dc645acb488a3e33707",
+    "url": "/struct/static/media/1.ac0bace5.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/struct/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/struct/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/struct/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/struct/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/struct/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
