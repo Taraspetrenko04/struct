979f6c512ed307f01d2be680a69e2d1b8f63dcf9
Taraspetrenko04 <Taraspetrenko04@gmail.com>
2020-04-13 17:08:54 +0300

    our projects make triangle hover

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
-import Header from '../Header/Header';
 import './style.scss';
+
+import Header from '../Header/Header';
+import Services from '../Services/Services';
+import OurProjects from '../OurProjects/OurProjects';
+import Footer from '../Footer/Footer';
+
 
 
 class App extends Component {
@@ -8,6 +13,9 @@ class App extends Component {
     return (
         <div className="container">
             <Header />
+            <Services />
+            <OurProjects />
+            <Footer />
         </div>
         
       )}
